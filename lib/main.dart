b3b9bb39cdff1e3857147b5c5a6e817f81e90e0b
xtine-op <xtineopwapo@gmail.com>
2023-11-25 19:16:48 +0300

    change font

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
-      theme: ThemeData(primarySwatch: Colors.orange),
+      theme: ThemeData(primarySwatch: Colors.orange, fontFamily: 'Lato'),
     );
   }
 }
